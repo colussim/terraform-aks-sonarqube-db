711581b00232bcbd39d1171265fca285cf87e26e
Emmanuel COLUSSI <emmanuel.colussi@hpe.com>
2022-10-28 17:42:20 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "1d7d141b-b12f-474e-8ef5-231f9a6e8367"
-  client_id       = "c5b5fe61-329b-4f88-a41c-18691ab72a88"
-  client_secret   = "Zzh8Q~4b1XYuUBsObxgEpL3AIfGULuWz9qHLEb~U" 
-  tenant_id       = "75622d57-788f-4dd3-b33c-739f625e6311"
+  subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  client_secret   = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 
